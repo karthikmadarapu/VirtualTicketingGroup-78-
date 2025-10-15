fdf0baf463129c997172182bad5ab0c198f0f59c
karthikmadarapu <karthikmadarapu21@gmail.com>
2025-10-15 16:38:10 -0400

    feat(config): wire up PostgreSQL connection and DbContext

--- VirtualTicketing/Program.cs
+++ VirtualTicketing/Program.cs
@@ -1,7 +1,14 @@
+using VirtualTicketing.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// DbContext (PostgreSQL)
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("PostgresConnection")));
 
 var app = builder.Build();
 
@@ -9,7 +16,6 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
