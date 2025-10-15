87768d0ba0b310217fdbfedc845cf3f4e9010b6b
karthikmadarapu <karthikmadarapu21@gmail.com>
2025-10-15 16:32:54 -0400

    feat(data): ApplicationDbContext with relationships

---
+++ VirtualTicketing/Data/ApplicationDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VirtualTicketing.Models;
+
+namespace VirtualTicketing.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+    : DbContext(options)
+{
+    public DbSet<Event> Events => Set<Event>();
+    public DbSet<Category> Categories => Set<Category>();
+    public DbSet<Purchase> Purchases => Set<Purchase>();
+    public DbSet<PurchaseItem> PurchaseItems => Set<PurchaseItem>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<PurchaseItem>()
+            .HasKey(pi => new { pi.PurchaseId, pi.EventId });
+
+        modelBuilder.Entity<PurchaseItem>()
+            .HasOne(pi => pi.Purchase)
+            .WithMany(p => p.Items)
+            .HasForeignKey(pi => pi.PurchaseId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<PurchaseItem>()
+            .HasOne(pi => pi.Event)
+            .WithMany(e => e.PurchaseItems)
+            .HasForeignKey(pi => pi.EventId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<Event>()
+            .HasOne(e => e.Category)
+            .WithMany(c => c.Events)
+            .HasForeignKey(e => e.CategoryId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
